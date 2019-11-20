84501e2e1dfa2dd99b9dffd3ed3b66d8d2120241
39786072 <santiagocorrao96@outlook.com>
2019-11-20 17:25:36 -0300

    Controlador ESP

--- app/controllers/esps_controller.rb
+++ app/controllers/esps_controller.rb
@@ -61,6 +61,22 @@ class EspsController < ApplicationController
     end
   end
 
+  def version 
+  
+  end
+
+  def sendData
+  
+  end
+
+  def updated
+  
+  end
+
+  def error
+  
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_esp
