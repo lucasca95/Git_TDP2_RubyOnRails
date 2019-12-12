e8be066b871e072a32a53458f3ffd820da448298
Santiago Corrao <santiagocorrao96@outlook.com>
2019-12-12 18:12:17 -0300

    Seeds Entrega

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,5 @@
 
 t = Target.create(name:"Arduino UNO")
 e = Esp.create(mac:"A0:20:A6:00:F3:CD", version:1)
-d = Device.create(name:"Arduino Catedra",latitude:0,longitude:0,esp_id:e.id,target_id:t.id)
-p = Program.create(name:"Blinky", description:"EL BLINKY QUE BLINKEA")
-p.targets << t
-v = Version.create(number:1,changelog:"500", program_id:p.id)
-p.versions << v
 i = Issue.create(message:"No hay más yerba")
 u = User.create(email:"santiagocorrao96@gmail.com",password:"123456")
-dv = Device.last.device_versions.build(version_id:1,state:1)
-dv.save
